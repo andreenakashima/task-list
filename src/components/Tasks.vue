a7a6ed0d0d4bd162ca144036d55eb9ca66109c2e
andreenakashima <andreenakashima@gmail.com>
2022-07-06 11:19:47 -0300

    Adicionado Header e Lista de tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-for="task in tasks">{{ task.text }}</div>
+</template>
+
+<script>
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array
+        }
+    }
+</script>
